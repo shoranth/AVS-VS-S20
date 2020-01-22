8d93667eb0fd68fbdba004f4ec8fcf2099fef97a
Anthony Shore <avshore@gmail.com>
2020-01-22 08:32:03 -0700

    Upload to Github. Fixed my problems.

---
+++ AskUserForName/AskUserForName.vb
@@ -0,0 +1,25 @@
+﻿'Anthony Shore
+'Visual Basic Coding Class
+'Spring 2020
+'Ask the user for their name and then respond assignment
+
+Module Module1
+
+    Sub Main()
+
+        'Declares variable to store user name
+        Dim userName As String
+
+        'Asks the user for their name
+        Console.WriteLine("What is your name? Please type it in below. Then press enter when finished. ")
+
+        'Stores the user's name in the userName variable
+        userName = Console.ReadLine()
+        
+        'Responds to the user with their name
+        Console.WriteLine("Thank you for getting me out of the Console " & userName & ". 10000 years will give you such a crick in the neck.")
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ MoreOperators/MoreOperators.vb
@@ -0,0 +1,28 @@
+﻿'Anthony Shore
+'RCET
+'Sring 2020
+'Math Operators 1-21-20
+
+Option Explicit On
+Option Strict On
+
+Module MoreOperators
+
+    Sub Main()
+        'prompt user for a message
+        'then display the message
+        Dim userMessage As String
+
+        'userMessage = "Hello"
+
+        Console.WriteLine("Please Enter a message. Press enter when done.")
+
+        userMessage = Console.ReadLine()
+
+        Console.WriteLine("The user enetered: " & Chr(34) & userMessage & Chr(34) & " Great Job User!")
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
