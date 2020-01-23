d19577d5223a86d9929c855c18f3a70cfd16a964
Anthony Shore <avshore@gmail.com>
2020-01-23 08:22:18 -0700

    create a basic if then else statement

---
+++ IfThen/IfThen.vb
@@ -0,0 +1,18 @@
+﻿Module IfThen
+
+    Sub Main()
+
+        'This is the complete if then statement
+        'If it evaluates to true, it will perform the code within the if then statement
+        'If false, it will skip the code in the if then statement
+        If 1 > 2 Then
+            Console.WriteLine("True")
+        Else
+            Console.WriteLine("False")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
