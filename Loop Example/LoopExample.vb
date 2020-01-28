069ec950b9f9a6d8d7497494b108d005e7585f01
Anthony Shore <avshore@gmail.com>
2020-01-28 08:39:14 -0700

    creates a basic for loop

---
+++ Loop Example/LoopExample.vb
@@ -0,0 +1,38 @@
+﻿'Anthony Shore
+'RCET 
+'Spring 2020
+'Loop Example
+
+
+Option Explicit On
+Option Strict On
+Option Compare Binary
+
+Module LoopExample
+
+    Sub Main()
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim result As Integer
+
+        firstNumber = 5
+        secondNumber = 7
+
+        result = firstNumber + secondNumber
+
+        For loopCount = 0 To 25 Step 5
+            result = result + 3
+            Console.WriteLine(loopCount)
+        Next
+
+        Console.WriteLine(result)
+
+        Console.ReadLine()
+
+
+
+
+    End Sub
+
+End Module
---
+++ Loop Example/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Loop Example")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Loop Example")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0c93a556-d01c-4082-9625-3ea08d9cb8d4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
