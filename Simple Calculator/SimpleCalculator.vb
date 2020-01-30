069189d58fbaad9e6ee70c9a87feb035bdfcfb33
Anthony Shore <avshore@gmail.com>
2020-01-30 08:57:58 -0700

    information passes between subs and functions

--- Simple Calculator/SimpleCalculator.vb
+++ Simple Calculator/SimpleCalculator.vb
@@ -4,6 +4,10 @@
 'Simple Calculator
 'https://github.com/shoranth/AVS-VS-S20
 
+Option Compare Text
+Option Strict On
+Option Explicit On
+
 Module SimpleCalculator
 
     Sub Main()
@@ -11,17 +15,26 @@ Module SimpleCalculator
         Dim firstNumber As Double
         Dim secondNumber As Double
         Dim usersOperation As Integer
+        Dim quitProgram As Boolean
 
+        quitProgram = False
+
+    Do While quitProgram = false
+
+        Do Until firstNumber = Double
         Console.WriteLine("This is a simple calculator.")
         Console.WriteLine("What is the FIRST number you would like in your calculation? Press enter when finished.")
         firstNumber = Console.ReadLine()
+        loop
 
+        Do Until secondNumber = Double
         Console.WriteLine("What is the SECOND number you would like in your calculation? Press enter when finished.")
         secondNumber = Console.ReadLine()
+        loop
 
         Console.WriteLine("Please select an operation for your calculation.")
         Console.WriteLine("1: Add")
-        Console.WriteLine("2: Subtract")
+        Console.WriteLine("2: Subtract") 
         Console.WriteLine("3: Multiply")
         Console.WriteLine("4: Divide")
         usersOperation = Console.ReadLine()
@@ -38,7 +51,15 @@ Module SimpleCalculator
             Console.WriteLine("The value you entered is invalid. Please try again.")
         End If
 
-        Console.ReadLine()
+        console.writeline("Do you want to quit the calculator? Press any key to run again. Press Q to quit, then press enter.")
+        If Console.ReadLine() = "q" Then 
+            quitProgram = True
+        Else
+            quitProgram = False 
+        End If
+        console.clear()
+
+    Loop
 
     End Sub
 
---
+++ SubFuctionExample/SubFunctionExample.vb
@@ -0,0 +1,52 @@
+﻿'Anthony Shore
+'RCET 
+'Spring 2020
+'Sub Function Example
+'
+
+Module SubFunctionExample
+
+    Sub Main()
+
+        Dim myNumber As Integer
+        myNumber = 7
+
+        Console.WriteLine("This is located in Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+        MySub(myNumber)
+        Console.WriteLine("This is located in Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+        myNumber = MyFunc(myNumber)
+        Console.WriteLine("This is located in Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+    End Sub
+
+    Sub MySub(myNumber As Integer)
+
+        'Dim myNumber As Integer
+        'myNumber = 2 + myNumber    same as the line of code below
+        myNumber += 2
+        Console.WriteLine("This is located in Sub MySub")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+    End Sub
+
+    Function MyFunc(myNumber As Integer) As Integer
+
+        'Dim myNumber As Integer
+        myNumber -= 93
+        Console.WriteLine("Located in Function MyFunc")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        Return myNumber
+
+    End Function
+
+End Module
