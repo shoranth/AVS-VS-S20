896c0f253005ee18817d12eb575a807345d333bf
Anthony Shore <avshore@gmail.com>
2020-02-04 08:54:30 -0700

    explores the properties and usefulness of subs

--- SubFuctionExample/SubFunctionExample.vb
+++ SubFuctionExample/SubFunctionExample.vb
@@ -2,27 +2,33 @@
 'RCET 
 'Spring 2020
 'Sub Function Example
-'
+
+Option Strict On
+Option Explicit On
 
 Module SubFunctionExample
 
     Sub Main()
 
-        Dim myNumber As Integer
-        myNumber = 7
+        'Dim myNumber As Integer
+        'myNumber = 7
 
-        Console.WriteLine("This is located in Sub Main")
-        Console.WriteLine(myNumber)
-        Console.ReadLine()
+        'Console.WriteLine("This is located in Sub Main")
+        'Console.WriteLine(myNumber)
+        'Console.ReadLine()
 
-        MySub(myNumber)
-        Console.WriteLine("This is located in Sub Main")
-        Console.WriteLine(myNumber)
-        Console.ReadLine()
+        'MySub(myNumber)
+        'Console.WriteLine("This is located in Sub Main")
+        'Console.WriteLine(myNumber)
+        'Console.ReadLine()
 
-        myNumber = MyFunc(myNumber)
-        Console.WriteLine("This is located in Sub Main")
-        Console.WriteLine(myNumber)
+        'myNumber = MyFunc(myNumber)
+        'Console.WriteLine("This is located in Sub Main")
+        'Console.WriteLine(myNumber)
+
+        'doMath(7, 5, "Subtract")
+
+        showPattern(4)
         Console.ReadLine()
 
     End Sub
@@ -36,6 +42,8 @@ Module SubFunctionExample
         Console.WriteLine(myNumber)
         Console.ReadLine()
 
+
+
     End Sub
 
     Function MyFunc(myNumber As Integer) As Integer
@@ -48,5 +56,37 @@ Module SubFunctionExample
         Return myNumber
 
     End Function
+
+    Sub doMath(firstNumber As Integer, secondNumber As Integer, operatorChoice As String)
+
+        Dim result As Double
+
+        If operatorChoice = "Add" Then
+            result = firstNumber + secondNumber
+        ElseIf operatorChoice = "Subtract" Then
+            result = firstNumber - secondNumber
+        ElseIf operatorChoice = "Multiply" Then
+            result = firstNumber * secondNumber
+        ElseIf operatorChoice = "Divide" Then
+            result = firstNumber / secondNumber
+        End If
+
+        Console.WriteLine(result)
+
+    End Sub
+
+    Sub showPattern(length As Integer)
+
+        Dim character As String
+        character = "*"
+
+        Dim pattern As String
+
+        For index = 0 To length
+            pattern &= character
+            Console.WriteLine(pattern)
+        Next
+
+    End Sub
 
 End Module
