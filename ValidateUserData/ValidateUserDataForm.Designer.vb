eeaad4958a079a29a3b43dc53241041863c8ce0f
Anthony Shore <avshore@gmail.com>
2020-02-13 08:54:18 -0700

    Checks if text boxes have text in them using a fucntion

---
+++ ValidateUserData/ValidateUserDataForm.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ValidateUserDataForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ValidateButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(122, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(151, 26)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(122, 90)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(151, 26)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(122, 122)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(151, 26)
+        Me.TextBox3.TabIndex = 2
+        '
+        'ValidateButton
+        '
+        Me.ValidateButton.Location = New System.Drawing.Point(122, 223)
+        Me.ValidateButton.Name = "ValidateButton"
+        Me.ValidateButton.Size = New System.Drawing.Size(151, 67)
+        Me.ValidateButton.TabIndex = 3
+        Me.ValidateButton.Text = "Validate"
+        Me.ValidateButton.UseVisualStyleBackColor = True
+        '
+        'ValidateUserDataForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(418, 355)
+        Me.Controls.Add(Me.ValidateButton)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "ValidateUserDataForm"
+        Me.Text = "Validate User Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ValidateButton As Button
+End Class
---
+++ ValidateUserData/ValidateUserDataForm.vb
@@ -0,0 +1,78 @@
+﻿Public Class ValidateUserDataForm
+    Private Sub ValidateUserDataForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ValidateButton_Click(sender As Object, e As EventArgs) Handles ValidateButton.Click
+        'IsEmpty()
+        Dim userMessage As String
+
+        If IsStringEmpty(TextBox3.Text) = True Then
+            userMessage &= "Please enter text in textbox 3." & vbNewLine
+            TextBox3.Select()
+        End If
+
+        If IsStringEmpty(TextBox2.Text) = True Then
+            userMessage &= "Please enter text in textbox 2." & vbNewLine
+            TextBox2.Select()
+        End If
+
+        If IsStringEmpty(TextBox1.Text) = True Then
+            userMessage &= "Please enter text in textbox 1." & vbNewLine
+            TextBox1.Select()
+        End If
+
+        If userMessage <> "" Then
+            MsgBox(userMessage)
+        End If
+    End Sub
+
+    'Checks to see if each text box is empty
+    Sub IsEmpty()
+
+        Dim userMessage As String
+        userMessage = ""
+
+        'Anthoer way to check a condition
+        'Select Case ""
+        '    Case TextBox3.Text
+        '        userMessage &= "Please enter text in textbox 3." & vbNewLine
+        '    Case TextBox2.Text
+        '        userMessage &= "Please enter text in textbox 2." & vbNewLine
+        '    Case TextBox1.Text
+        '        userMessage &= "Please enter text in textbox 1." & vbNewLine
+        'End Select
+
+        'If TextBox1.Text = "" Then
+        '    userMessage &= "Please enter text in textbox 1." & vbNewLine
+        '    'selects the box that needs to be filled in when this if is triggered
+        '    TextBox1.Select()
+        'End If
+
+        'If TextBox2.Text = "" Then
+        '    userMessage &= "Please enter text in textbox 2." & vbNewLine
+        '    TextBox2.Select()
+        'End If
+
+        'If TextBox3.Text = "" Then
+        '    userMessage &= ("Please enter text in texbox 3.")
+        '    TextBox3.Select()
+        'End If
+
+        'If userMessage <> "" Then
+        '    MsgBox(userMessage)
+        'End If
+
+    End Sub
+
+    Function IsStringEmpty(stringUnderTest As String) As Boolean
+        Dim status As Boolean
+        If stringUnderTest = "" Then
+            status = True
+        Else
+            status = False
+        End If
+        Return status
+    End Function
+
+End Class
