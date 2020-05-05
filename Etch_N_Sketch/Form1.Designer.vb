2bcd898f041a6058d939bb2b671909e8071d0d52
Anthony Shore <avshore@gmail.com>
2020-05-05 15:21:05 -0600

    adds buttons for the user to choose cursor color

--- Etch_N_Sketch/Form1.Designer.vb
+++ Etch_N_Sketch/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Etch_N_Sketch
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,33 +20,83 @@ Partial Class Etch_N_Sketch
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ColorPicker = New System.Windows.Forms.ColorDialog()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CursorColorPicker = New System.Windows.Forms.ColorDialog()
+        Me.DrawAreaLabel = New System.Windows.Forms.Label()
+        Me.EraseButton = New System.Windows.Forms.Button()
+        Me.CursorColorButton = New System.Windows.Forms.Button()
+        Me.BackgroundColorButton = New System.Windows.Forms.Button()
+        Me.BackgroundColorPicker = New System.Windows.Forms.ColorDialog()
+        Me.ExitButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'DrawAreaLabel
         '
-        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label1.Location = New System.Drawing.Point(179, 36)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(402, 236)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.DrawAreaLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DrawAreaLabel.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.DrawAreaLabel.Location = New System.Drawing.Point(179, 91)
+        Me.DrawAreaLabel.Name = "DrawAreaLabel"
+        Me.DrawAreaLabel.Size = New System.Drawing.Size(380, 158)
+        Me.DrawAreaLabel.TabIndex = 0
+        '
+        'EraseButton
+        '
+        Me.EraseButton.Location = New System.Drawing.Point(257, 306)
+        Me.EraseButton.Name = "EraseButton"
+        Me.EraseButton.Size = New System.Drawing.Size(75, 23)
+        Me.EraseButton.TabIndex = 1
+        Me.EraseButton.Text = "Erase"
+        Me.EraseButton.UseVisualStyleBackColor = True
+        '
+        'CursorColorButton
+        '
+        Me.CursorColorButton.Location = New System.Drawing.Point(420, 306)
+        Me.CursorColorButton.Name = "CursorColorButton"
+        Me.CursorColorButton.Size = New System.Drawing.Size(75, 23)
+        Me.CursorColorButton.TabIndex = 2
+        Me.CursorColorButton.Text = "Cursor Color"
+        Me.CursorColorButton.UseVisualStyleBackColor = True
+        '
+        'BackgroundColorButton
+        '
+        Me.BackgroundColorButton.Location = New System.Drawing.Point(546, 306)
+        Me.BackgroundColorButton.Name = "BackgroundColorButton"
+        Me.BackgroundColorButton.Size = New System.Drawing.Size(75, 23)
+        Me.BackgroundColorButton.TabIndex = 3
+        Me.BackgroundColorButton.Text = "Background Color"
+        Me.BackgroundColorButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(644, 306)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
         '
         'Etch_N_Sketch
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.BackgroundColorButton)
+        Me.Controls.Add(Me.CursorColorButton)
+        Me.Controls.Add(Me.EraseButton)
+        Me.Controls.Add(Me.DrawAreaLabel)
         Me.Name = "Etch_N_Sketch"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents ColorPicker As ColorDialog
-    Friend WithEvents Label1 As Label
+    Friend WithEvents CursorColorPicker As ColorDialog
+    Friend WithEvents DrawAreaLabel As Label
+    Friend WithEvents EraseButton As Button
+    Friend WithEvents CursorColorButton As Button
+    Friend WithEvents BackgroundColorButton As Button
+    Friend WithEvents BackgroundColorPicker As ColorDialog
+    Friend WithEvents ExitButton As Button
 End Class
--- Etch_N_Sketch/Form1.vb
+++ Etch_N_Sketch/Form1.vb
@@ -6,12 +6,59 @@
 Public Class Etch_N_Sketch
 
 
-    Private Sub Etch_N_Sketch_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+    'Private Sub Etch_N_Sketch_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+    '    Me.Text = "x: " & e.X & "y: " & e.Y & "Mouse Button: " & e.Button.ToString
+
+    '    'create graphics object
+    '    Dim graph As Graphics
+    '    graph = Label1.CreateGraphics
+
+    '    Static penColor As Color
+
+    '    If penColor.IsEmpty = True Then
+    '        penColor = Color.Black
+    '    End If
+
+    '    Dim myPen As New Pen(penColor)
+
+    '    'contructor for pen object
+    '    Static oldX As Integer
+    '    Static oldY As Integer
+
+
+
+
+    '    If e.Button.ToString = "Left" Then
+    '        'draw the line
+    '        graph.DrawLine(myPen, oldX, oldY, e.X, e.Y)
+    '    ElseIf e.Button.ToString = "Right" Then
+    '        graph.Clear(Color.Gray)
+    '    ElseIf e.Button.ToString = "Middle" Then
+    '        ColorPicker.ShowDialog()
+    '        penColor = ColorPicker.Color
+    '        myPen.Color = ColorPicker.Color
+    '    End If
+
+    '    'store position
+    '    oldX = e.X
+    '    oldY = e.Y
+
+    '    'free up resources
+    '    myPen.Dispose()
+    '    graph.Dispose()
+    'End Sub
+
+
+    Dim penColor As Color
+    Dim myPen As New Pen(penColor)
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawAreaLabel.MouseMove
+
         Me.Text = "x: " & e.X & "y: " & e.Y & "Mouse Button: " & e.Button.ToString
 
         'create graphics object
         Dim graph As Graphics
-        graph = Me.CreateGraphics
+        graph = DrawAreaLabel.CreateGraphics
 
         Static penColor As Color
 
@@ -25,19 +72,19 @@ Public Class Etch_N_Sketch
         Static oldX As Integer
         Static oldY As Integer
 
-
-
-
         If e.Button.ToString = "Left" Then
             'draw the line
             graph.DrawLine(myPen, oldX, oldY, e.X, e.Y)
         ElseIf e.Button.ToString = "Right" Then
-            graph.Clear(Color.Gray)
+            DrawAreaLabel.CreateGraphics.Clear(Color.FloralWhite)
         ElseIf e.Button.ToString = "Middle" Then
-            ColorPicker.ShowDialog()
-            penColor = ColorPicker.Color
-            myPen.Color = ColorPicker.Color
+
         End If
+
+
+        penColor = CursorColorPicker.Color
+        myPen.Color = CursorColorPicker.Color
+        DrawAreaLabel.BackColor = BackgroundColorPicker.Color
 
         'store position
         oldX = e.X
@@ -46,6 +93,34 @@ Public Class Etch_N_Sketch
         'free up resources
         myPen.Dispose()
         graph.Dispose()
+    End Sub
+
+    Private Sub EraseButton_Click(sender As Object, e As EventArgs) Handles EraseButton.Click
+
+        DrawAreaLabel.CreateGraphics.Clear(Color.FloralWhite)
+
+    End Sub
+
+    Private Sub CursorColorButton_Click(sender As Object, e As EventArgs) Handles CursorColorButton.Click
+        CursorColorPicker.ShowDialog()
+        penColor = CursorColorPicker.Color
+        myPen.Color = CursorColorPicker.Color
+    End Sub
+
+    Private Sub BackgroundColorButton_Click(sender As Object, e As EventArgs) Handles BackgroundColorButton.Click
+
+        BackgroundColorPicker.ShowDialog()
+
+    End Sub
+
+    Private Sub Etch_N_Sketch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DrawAreaLabel.CreateGraphics.Clear(Color.FloralWhite)
+        BackgroundColorPicker.Color = Color.FloralWhite
+        DrawAreaLabel.BackColor = BackgroundColorPicker.Color
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
     End Sub
 
 End Class
